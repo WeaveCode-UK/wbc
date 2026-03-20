1358def4ff9c36bbf26e25f8336997a9046d5bb8
Seu Nome <seu-email@exemplo.com>
2026-03-20 21:22:10 +0000

    fix(tests): fix import in client value-objects test

--- packages/business/clients/domain/__tests__/value-objects.test.ts
+++ packages/business/clients/domain/__tests__/value-objects.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { validatePhone, formatPhoneE164, calculateClassification } from '../value-objects';
+import { validatePhone, formatPhoneE164 } from '../value-objects';
 import { calculateClassification as calcClass } from '../entities';
 
 describe('Client Value Objects', () => {
